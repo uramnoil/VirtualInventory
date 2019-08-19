58c1cc1a55c5ce100d07ef997ab9e4cd18b0d4b5
UramnOIL <uramnoil@outlook.jp>
2019-08-20 03:26:52 +0900

    DoubleChestImpersonatorの実装.

--- src/uramnoil/virtualchest/disguiser/DoubleChestImpersonator.php
+++ src/uramnoil/virtualchest/disguiser/DoubleChestImpersonator.php
@@ -5,9 +5,12 @@ namespace uramnoil\virtualchest\disguiser;
 
 use pocketmine\block\BlockFactory;
 use pocketmine\block\BlockIds;
-use pocketmine\block\Chest;
+use pocketmine\nbt\NetworkLittleEndianNBTStream;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\network\mcpe\protocol\BlockEntityDataPacket;
 use pocketmine\Player;
 use uramnoil\virtualchest\inventory\VirtualChestInventory;
+use pocketmine\tile\Chest;
 
 class DoubleChestImpersonator extends ChestImpersonator {
 	/** @var Chest */
@@ -18,18 +21,50 @@ class DoubleChestImpersonator extends ChestImpersonator {
 	public function __construct(Player $impersonated, VirtualChestInventory $inventory) {
 		parent::__construct($impersonated, $inventory);
 		$this->chest1 = BlockFactory::get(BlockIds::CHEST, null, $this->basedPosition);
-		$this->chest2 = BlockFactory::get(BlockIds::CHEST, null, $this->basedPosition->add);
+		$this->chest2 = BlockFactory::get(BlockIds::CHEST)->setComponents(
+			$this->basedPosition->x + 1,
+			$this->basedPosition->y,
+			$this->basedPosition->z
+		);
 	}
 
 	protected function sendChestBlocks() : void {
-		// TODO: Implement sendChestBlocks() method.
-	}
+		$this->impersonated->level->sendBlocks([$this->impersonated], [$this->chest1]);
+		$this->impersonated->level->sendBlocks([$this->impersonated], [$this->chest2]);
 
-	protected function sendContainerPacket() : void {
-		// TODO: Implement sendContainerPacket() method.
+		$this->replacedPositions[] = $this->chest1;
+		$this->replacedPositions[] = $this->chest2;
 	}
 
 	protected function sendTilePacket() : void {
-		// TODO: Implement sendTilePacket() method.
+		$writer = new NetworkLittleEndianNBTStream();
+
+		//$chest1
+		$pk = new BlockEntityDataPacket();
+		$pk->x = $this->chest1->x;
+		$pk->y = $this->chest1->y;
+		$pk->z = $this->chest1->z;
+
+		$nbt = new CompoundTag();
+		$nbt->setInt(Chest::TAG_PAIRX, $this->chest2->x);
+		$nbt->setInt(Chest::TAG_PAIRZ, $this->chest2->z);
+
+		$pk->namedtag = $writer->write($nbt);
+
+		$this->impersonated->dataPacket($pk);
+
+		//chest2
+		$pk = new BlockEntityDataPacket();
+		$pk->x = $this->chest2->x;
+		$pk->y = $this->chest2->y;
+		$pk->z = $this->chest2->z;
+
+		$nbt = new CompoundTag();
+		$nbt->setInt(Chest::TAG_PAIRX, $this->chest1->x);
+		$nbt->setInt(Chest::TAG_PAIRZ, $this->chest1->z);
+
+		$pk->namedtag = $writer->write($nbt);
+
+		$this->impersonated->dataPacket($pk);
 	}
 }
